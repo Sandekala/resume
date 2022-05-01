d6c810605dea1ffe4e6df339fb5f1c17d9a1befb
Dimas Abdul Aziz <diimasaziiz@gmail.com>
2022-05-01 16:02:17 +0700

    first commit

---
+++ application/views/Resume.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Resume - Dimas Abdul Aziz 2022</title>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/style.css' ?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/bootstrap.css' ?>">
+    </head>
+    <body>
+        <header>            
+            <h3 class="nama"><----------------Resume Online----------------></h3>         
+        </header>
+        <div id=sidebar class="title">
+            <img class="foto" src="<?php echo base_url(); ?>assets/images/dimas.png">
+            <br>
+            <br>
+            <div class="title">
+            <font size="15" color="white"><b>Profil</b></font>
+        </div>
+        <div class="content">
+            <p style="text-indent: 45px;">Berpengalaman di bidang Administrasi selama lebih dari 4 tahun, menyukai pekerjaan yang bersifat Back Office juga sangat baik dalam mengolah data degan terstruktur dan berpengalaman sebagai Asisten Supervisor / Koordinator selama hampir 2 tahun.</p>
+        <hr>
+        </div>
+        <div class="title">
+            <font size="15" color="white"><b>Pendidikan</b></font>
+        </div>
+        <div class="content">
+            <li>Universitas Bina Sarana Informatika</li>
+            <p>Diploma Sistem Informasi | 2017 - 2020</p>
+            <p>IPK 3.12 / 4</p><br>
+            <li>SMAN 1 Banjarsari</li>
+            <p>Jurusan IPS | 2011 - 2014</p>
+            <hr>
+        </div>
+        <div class="title">
+            <font size="15" color="white"><b>Sertifikat</b></font>
+        </div>
+        <div class="content">
+            <li>Sertifikat Kompetensi Pemrogram</li>
+            <p>Pengembang Perangkat Lunak | 2020 - 2023</p>
+            <hr>
+        </div>
+        <div class="title">
+            <font size="15" color="white"><b>Kontak</b></font>
+        </div>
+        <div class="content">
+            <li>Email : diimasaziiz@gmail.com</li>
+            <li>Telp : 082214554455</li>
+            <hr>
+        </div>
+    </div>
+        <div id=description>
+            <div class="title">
+            <font size="10"><b>Dimas Abdul Aziz</b></font>
+            <hr color="black">
+            <div class="title">
+            <h3><b>ADMINISTRATOR</b></h3>
+            <hr color="black">
+            </div>
+            <div class="latar">
+                <div class="content-desc">
+                    <h3><b>Pengalaman Kerja</b></h3>
+                </div>
+            </div>
+        </div>
+    </body>
